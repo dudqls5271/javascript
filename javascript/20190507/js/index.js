095683378fd70a123bd9eca01bf1151e22b303e6
dudqls5271 <49426352+dudqls5271@users.noreply.github.com>
2019-05-28 15:11:43 +0900

    javascript

--- javascript/20190507/js/index.js
+++ javascript/20190507/js/index.js
@@ -14,9 +14,16 @@ let car = document.getElementById("car");
 let title = document.getElementById("title");
 let name = document.getElementById("name");
 const delButt = document.querySelectorAll(".delBtn");
-const modifyBtn = document.querySelectorAll(".modifyBtn");
+const modifyBtn = document.getElementsByClassName("modifyBtn");
+const checkDelBtn = document.querySelector(".checkDelBtn");
+const my_tbody = document.querySelector('.boardTable > tbody');
+const row_num = document.querySelector("#row_num");
+
+//체크박스
+const checkbox = document.getElementsByClassName("checkbox");
 
 const modalWriteTitle = document.querySelector(".write_section > h1");
+
 
 function timeSetting() {
     let date = new Date();
@@ -34,6 +41,9 @@ writeBtn.addEventListener("click", function () {
         modalWriteBox.classList.remove("hidden");
     }
     modalWriteTitle.innerText = "등록";
+    car.value = "";
+    title.value = "";
+    name.value = "";
 });
 modalWriteBoxBody.addEventListener("click", function () {
     if (!modalWriteBox.classList.contains("hidden")) {
@@ -47,44 +57,79 @@ modalWriteCloseBtn.addEventListener("click", function () {
 });
 
 modalWriteSubmitBtn.addEventListener("click", function () {
-    if (car.value == "" || name.value == "" || title.value == "") {
-        alert("공백 입니다. 다시 입력해주세요")
+    if (modalWriteTitle.innerText == "등록") {
+        if (car.value == "" || name.value == "" || title.value == "") {
+            alert("공백 입니다. 다시 입력해주세요")
+        } else {
+
+            modalWriteBox.classList.add("hidden");
+            var row = my_tbody.insertRow(1); // 상단에 추가
+            //var row = my_tbody.insertRow(my_tbody.rows.length); // 하단에 추가
+            var cell1 = row.insertCell();
+            var cell2 = row.insertCell();
+            var cell3 = row.insertCell();
+            var cell4 = row.insertCell();
+            var cell5 = row.insertCell();
+            var cell6 = row.insertCell();
+            cell1.innerHTML = "<input type=checkbox class='checkbox' data-num='" + (my_tbody.rows.length - 1) + "'></input>";
+            cell2.innerHTML = car.value;
+            cell3.innerHTML = title.value;
+            cell4.innerHTML = name.value;
+            cell5.innerHTML = timeSetting();
+            cell6.innerHTML = "<input type='button' value='수정' class='modifyBtn'><input type='button' value='삭제' class='delBtn'>";
+        }
     } else {
-        var my_tbody = document.querySelector('.boardTable > tbody');
-        modalWriteBox.classList.add("hidden");
-        var row = my_tbody.insertRow(1); // 상단에 추가
-        //var row = my_tbody.insertRow(my_tbody.rows.length); // 하단에 추가
-        var cell1 = row.insertCell();
-        var cell2 = row.insertCell();
-        var cell3 = row.insertCell();
-        var cell4 = row.insertCell();
-        var cell5 = row.insertCell();
-        var cell6 = row.insertCell();
-        cell1.innerHTML = "<input type=checkbox></input>";
-        cell2.innerHTML = car.value;
-        cell3.innerHTML = title.value;
-        cell4.innerHTML = name.value;
-        cell5.innerHTML = timeSetting();
-        cell6.innerHTML = "<input type='button' value='수정' class='modifyBtn'><input type='button' value='삭제' class='delBtn'>";
+        let car_re = car.value;
+        let title_re = title.value;
+        let name_re = name.value;
+        console.log(row_num.value);
+        for (let i = 0; i < checkbox.length; i++) {
+            if (checkbox[i].getAttribute("data-num") == row_num.value) {
+                const selectTr = checkbox[i].parentElement.parentElement;
+
+                selectTr.cells[1].innerText = car_re;
+                selectTr.cells[2].innerText = title_re;
+                selectTr.cells[3].innerText = name_re;
+            }
+        }
     }
+    modalWriteBox.classList.add("hidden");
 })
 
 //---------------------수정 div------------------------------
+// console.log(modifyBtn);
+// Array.from(modifyBtn).forEach(btn => {
+//     console.log(btn);
+//     btn.addEventListener("click", function () {
+//         if (modalWriteBox.classList.contains("hidden")) {
+//             modalWriteBox.classList.remove("hidden");
+//             // const selectTr = event.target.parentElement.parentElement;
+//             // console.dir(selectTr);
+//             // selectTr.remove();
+//         }
+//         const selectTr = event.target.parentElement.parentElement;
+//         console.dir(selectTr);
 
-modifyBtn.forEach(btn => {
-    btn.addEventListener("click", function () {
-        if (modalWriteBox.classList.contains("hidden")) {
-            modalWriteBox.classList.remove("hidden");
-        }
-    });
-});
+//         const categoryStr = selectTr.cells[1].innerText;
+//         const titleStr = selectTr.cells[2].innerText;
+//         const nemeStr = selectTr.cells[3].innerText;
+//         console.log(categoryStr);
 
-modalWriteCloseBtn_re.addEventListener("click", function () {
+//         modalWriteTitle.innerText = "수정";
+//         car.value = categoryStr;
+//         title.value = titleStr;
+//         name.value = nemeStr;
+
+//     });
+
+// });
+
+modalWriteBoxBody.addEventListener("click", function () {
     if (!modalWriteBox.classList.contains("hidden")) {
         modalWriteBox.classList.add("hidden");
     }
 });
-modalBox_modifyBtn_body.addEventListener("click", function () {
+modalWriteCloseBtn.addEventListener("click", function () {
     if (!modalWriteBox.classList.contains("hidden")) {
         modalWriteBox.classList.add("hidden");
     }
@@ -92,27 +137,61 @@ modalBox_modifyBtn_body.addEventListener("click", function () {
 
 
 //------------------삭제----------------------------------
-delButt.forEach(btn => {
-    console.dir(btn);
-    console.log(btn.childElementCount);
-    btn.addEventListener("click", function () {
-        var my_tbody = document.querySelector('.boardTable > tbody');
-        if (my_tbody.rows.length < 1) return;
-        my_tbody.deleteRow(1); // 상단부터 삭제
-        my_tbody.deleteRow(); // 하단부터 삭제
-        console.log(this);
+document.body.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delBtn")) {
+        console.log("삭제 클릭");
+        const selectTr = event.target.parentElement.parentElement;
+        console.dir(selectTr);
+        selectTr.remove();
+    }
+    //----------수정2--------------------------
+    if (event.target.classList.contains("modifyBtn")) {
+        modalWriteBox.classList.remove("hidden");
+        console.log("수정");
+        modalWriteTitle.innerText = "수정";
+        const selectTr = event.target.parentElement.parentElement;
+        console.dir(selectTr);
 
-    });
+        const categoryStr = selectTr.cells[1].innerText;
+        const titleStr = selectTr.cells[2].innerText;
+        const nemeStr = selectTr.cells[3].innerText;
+        console.log(categoryStr);
+
+        car.value = categoryStr;
+        title.value = titleStr;
+        name.value = nemeStr;
+        row_num.value = selectTr.cells[0].firstChild.getAttribute("data-num");
+        console.dir(selectTr.cells[0].firstChild);
+        console.dir(selectTr.cells[0].firstChild.dataset.num);
+    }
+
 });
-/*
-delButt.forEach(function (btn, idx) {
-    //console.dir(btn);
-    //console.log(btn.childElementCount);
-    btn.addEventListener("click", function () {
-        var my_tbody = document.querySelector('.boardTable > tbody');
-        if (my_tbody.rows.length < 1) return;
-        // my_tbody.deleteRow(0); // 상단부터 삭제
-        my_tbody.deleteRow(idx + 1); // 하단부터 삭제
-    });
-});*/
 
+//-------------선택삭제-------------------------------
+// document.body.addEventListener("click", function (event) {
+//     if (event.target.classList.contains("checkbox")) {
+//         console.log("체크");
+//         const selectTr = event.target.parentElement.parentElement;
+//         console.dir(selectTr);
+//         selectTr.remove();
+//     }
+// });
+
+checkDelBtn.addEventListener("click", function () {
+    for (let i = checkbox.length - 1; i >= 0; i--) {
+        if (checkbox[i].checked) {
+            console.dir(checkbox[i])
+            checkbox[i].parentElement.parentElement.remove();
+        }
+    }
+    // if (event.target.classList.contains("checkbox")) {
+    //     console.log("체크");
+    //     const selectTr = event.target.parentElement.parentElement;
+    //     console.dir(selectTr);
+    //     selectTr.remove();
+    // }
+});
+
+// checkbox.addEventListener("click", function () {
+//     console.dir(checkbox);
+// });
